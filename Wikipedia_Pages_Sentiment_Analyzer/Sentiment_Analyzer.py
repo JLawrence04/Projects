6e713a97d027bc0508fb71eb03e6aa4ff601f521
jolawre <jolawre@iu.edu>
2024-08-18 18:26:00 -0400

    Added a way to find polarities of different sentence vector cluster groups

--- Wikipedia_Pages_Sentiment_Analyzer/Sentiment_Analyzer.py
+++ Wikipedia_Pages_Sentiment_Analyzer/Sentiment_Analyzer.py
@@ -4,7 +4,10 @@ import json
 import re
 from textblob import TextBlob
 import contractions
-
+from gensim.models import Word2Vec
+import numpy as np
+from sklearn.cluster import KMeans
+from kneed import KneeLocator
 def find_page(input):
     url = 'https://en.wikipedia.org/w/rest.php/v1/search/title' #API endpoints needed were found on Wikipedia's MediaWiki REST API
     info = requests.get(url, params= {'q': input, 'limit':1})
@@ -40,14 +43,81 @@ def clean_text(text):
 
     text = re.sub("\n", " ", text) #\n indicates a line break and isn't needed for the sentiment analysis
 
-    text = re.sub(r'[^a-zA-Z\s]', '', text) #Removing special characters, punctuation, and numbers that will just add noise to sentiment analysis
+    sentences = TextBlob(text).sentences
+    
+    lst = []
+    for i in range(len(sentences)):
+        sentence = sentences[i]
+        sentence = re.sub(r'[^a-zA-Z\s]', '', str(sentence)) #Removing special characters, punctuation, and numbers that will just add noise to sentiment analysis
 
-    text = re.sub('\t', "", text)
+        sentence = re.sub('\t', "", sentence)
 
-    text = re.sub(r"\s{2,}", " ", text)
-    return text
-print([find_page('basket')[2]])
+        sentence = re.sub(r"\s{2,}", " ", sentence)
+        lst.append(sentence)
+    return lst
 
-x = [find_page('basket')[2]]
+def encode(sentences):
+    #Splitting sentences into words for encoding but still listed with words in same sentence
+    words = [sentence.split() for sentence in sentences]
 
-print([clean_text(x[0])])
+    encode_model = Word2Vec(words,window=5, min_count=1)
+
+    vectors = []
+    for sentence in words:
+        word_vectors = []
+        for word in sentence:
+            if word in encode_model.wv:
+                word_vectors.append(encode_model.wv[word])
+        vectors.append(np.mean(word_vectors, axis = 0)) #Averaging the word vectors to create vector for the entire sentence
+    
+    return vectors
+
+def clusters(vectors):
+
+# Calculate WCSS for different numbers of clusters and automatically detect the elbow point
+    wcss = []
+    count = 0
+    for i in range(1, 11):
+        if i <= len(vectors):
+            kmeans = KMeans(n_clusters=i, random_state=0)
+            kmeans.fit(vectors)
+            wcss.append(kmeans.inertia_)
+            count += 1
+
+
+    kneedle = KneeLocator(range(1, count+1), wcss, curve='convex', direction='decreasing')
+    optimal_clusters = kneedle.elbow
+    return optimal_clusters
+
+def sentiment(sentences,text):
+    if len(sentences) >= 2 and (clusters(sentences) != None):
+        clusters_num = clusters(sentences)
+        kmeans = KMeans(n_clusters=clusters_num, random_state=0)
+        kmeans.fit(sentences)
+
+        labels = kmeans.labels_
+        clusters_dict = {i: [] for i in range(clusters_num)}
+
+        #Storing sentences vectors in dictionary under the cluster they were labeled
+        for i,sentence in enumerate(text):
+            clusters_dict[labels[i]].append(sentence)
+        
+        #Finding polarity scores of each cluster, can show how much weight each cluster has in the overall polarity score
+        cluster_polarity = {}
+        for i in range(clusters_num):
+            polarity = TextBlob(" ".join(clusters_dict[i])).sentiment.polarity
+            cluster_polarity[i] = polarity
+            
+
+        return [TextBlob(" ".join(text)).sentiment.polarity,TextBlob(" ".join(text)).sentiment.subjectivity,cluster_polarity]
+    else:
+        return [TextBlob(" ".join(text)).sentiment.polarity,TextBlob(" ".join(text)).sentiment.subjectivity]
+
+x = [find_page('basketball')[2]]
+
+x = clean_text(x[0])
+
+print('===================================')
+y = encode(x)
+
+print(sentiment(y,x))
